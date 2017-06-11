ee9382d0cbec8e2bad5d7d91237c00902a9be51c
Matt <matt@mtmc.ca>
2017-06-11 16:32:32 -0600

    Rough layout and inital stripe

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+require_once('vendor/autoload.php');
+$stripe = array(
+  secret_key      => getenv('secret_key'),
+  publishable_key => getenv('publishable_key')
+);
+\Stripe\Stripe::setApiKey($stripe['secret_key']);
+?>
